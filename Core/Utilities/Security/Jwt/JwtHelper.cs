19d7d0f33eef43951631882072a0c382d5cf443b
rahmaniftaliev <rahmaniftaliev@gmail.com>
2026-08-04 19:03:46 +0400

    Launching the JWT infrastructure

---
+++ Core/Utilities/Security/Jwt/JwtHelper.cs
@@ -0,0 +1,26 @@
+﻿using Core.Entities.Concrete;
+using Core.Utilities.Security.Encryption;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Security.Jwt
+{
+    public class JwtHelper : ITokenHelper
+    {
+        public IConfiguration Configuration { get; }
+
+        private TokenOptions _tokenOptions;
+
+        public JwtHelper(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            _tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
+        }
+        public AccessToken CreateToken(User user, List<OperationClaim> operationClaims)
+        {
+           var securityKey = SecurityKeyHelper.CreateSecurityKey(_tokenOptions.SecurityKey);
+        }
+    }
+}
