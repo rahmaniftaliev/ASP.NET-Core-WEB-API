971d7fc4bdc50b84152a9801d80cd80bc7eac1dd
rahmaniftaliev <rahmaniftaliev@gmail.com>
2026-08-04 17:09:19 +0400

    Code Refactoring

--- Core/Utilities/Security/Jwt/ITokenHelper.cs
+++ Core/Utilities/Security/Jwt/ITokenHelper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +7,6 @@ namespace Core.Utilities.Security.Jwt
 {
     public interface ITokenHelper
     {
-        AccessToken CreateToken();
+        AccessToken CreateToken(User user,List<OperationClaim> operationClaims);
     }
 }
