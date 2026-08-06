b014d3514e92ffcb8475678253e34f159ac9d8b3
rahmaniftaliev <rahmaniftaliev@gmail.com>
2026-08-06 19:27:46 +0400

    Writing the authentication service

---
+++ Business/Concrete/AuthManager.cs
@@ -0,0 +1,43 @@
+﻿using Business.Abstract;
+using Core.Entities.Concrete;
+using Core.Utilities.Results;
+using Core.Utilities.Security.Jwt;
+using Entities.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class AuthManager : IAuthService
+    {
+        private IUserService _userService;
+        private ITokenHelper _tokenHelper;
+
+        public AuthManager(IUserService userService, ITokenHelper tokenHelper)
+        {
+            _userService = userService;
+            _tokenHelper = tokenHelper;
+        }
+
+        public IDataResult<AccessToken> CreateAccessToken(User user)
+        {
+            
+        }
+
+        public IDataResult<User> Login(UserForLoginDto userForLoginDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<User> Register(UserForRegisterDto userForRegisterDto, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult UserExists(string email)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
