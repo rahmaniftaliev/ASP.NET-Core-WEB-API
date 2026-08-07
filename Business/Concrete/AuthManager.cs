9bf07140d753e301cf9d932b1622bc2b97d627c1
rahmaniftaliev <rahmaniftaliev@gmail.com>
2026-08-07 18:35:50 +0400

    Writing login and register APIs

--- Business/Concrete/AuthManager.cs
+++ Business/Concrete/AuthManager.cs
@@ -43,7 +43,7 @@ namespace Business.Concrete
                 return new ErrorDataResult<User>(Messages.PasswordError);
             }
 
-            return new SuccessDataResult<User>(userToCheck, Messages.SuccessfulLogin)
+            return new SuccessDataResult<User>(userToCheck, Messages.SuccessfulLogin);
         }
 
         public IDataResult<User> Register(UserForRegisterDto userForRegisterDto, string password)
